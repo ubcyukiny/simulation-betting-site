c79fdba7b672fd42346e1468ed7d8a3facf1ec37
Andrew <Andrew-black@outlook.com>
2023-07-27 13:06:02 -0700

    Added more example scripts and files

--- Python/nba_games_info.py
+++ Python/nba_games_info.py
@@ -5,13 +5,30 @@ from datetime import datetime
 # Get current year
 current_year = datetime.now().year
 
-# Get games data from NBA API for the current season
-game_finder = leaguegamefinder.LeagueGameFinder(season_nullable=current_year)
+# Get games data from NBA API for the 2022-23 regular season
+game_finder = leaguegamefinder.LeagueGameFinder(season_nullable='2022-23')
 games = game_finder.get_data_frames()[0]
 
-# Convert data to DataFrame
-games_df = pd.DataFrame(games)
+# Separate games data for home and away teams
+home_games = games[games['MATCHUP'].str.contains('@')]
+away_games = games[~games['MATCHUP'].str.contains('@')]
+
+# Merge home and away games on 'GAME_ID'
+games_combined = pd.merge(home_games, away_games, on='GAME_ID', suffixes=('_Home', '_Away'))
+
+# Extract only the essential columns
+games_combined = games_combined[['GAME_ID', 'PTS_Home', 'PTS_Away', 'GAME_DATE_Home', 'TEAM_ID_Home', 'TEAM_ID_Away']]
+
+# Rename the columns
+games_combined = games_combined.rename(columns={
+    'GAME_ID': 'GameID',
+    'PTS_Home': 'ScoreHome',
+    'PTS_Away': 'ScoreAway',
+    'GAME_DATE_Home': 'Date',
+    'TEAM_ID_Home': 'HomeTeamID',
+    'TEAM_ID_Away': 'AwayTeamID'
+})
 
 # Export to Excel
-output_file = 'nba_games_this_season.xlsx'
-games_df.to_excel(output_file, index=False, engine='openpyxl')
+output_file = 'nba_games_this_season_combined.xlsx'
+games_combined.to_excel(output_file, index=False, engine='openpyxl')
