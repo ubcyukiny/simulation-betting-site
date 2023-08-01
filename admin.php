73b4a6c1287a1f27da0ac18dbad8d008e117d2b7
ubcyukiny <yukiny@student.ubc.ca>
2023-07-31 19:55:19 -0700

    User can sign up on main, will get warnings if using a used username or email, Admin can view list of users in admin.php

--- admin.php
+++ admin.php
@@ -1,27 +1,105 @@
-
 <html>
 <head>
     <title>304 Project</title>
 </head>
 <body>
 <h1>This is the admin page</h1>
-<h1>Lists of current users</h1>
-<p><input type="submit" value="Display" name="DisplayCurrUsers"></p>
+<h1>Lists of current users(Working?)</h1>
+<form method="GET" action="admin.php"> <!--refresh page when submitted-->
+    <input type="hidden" id="DisplayCurrUsersRequest" name="DisplayCurrUsersRequest">
+    <input type="submit" value="Display" name="DisplayCurrUsers">
+</form>
 <hr/>
-<h1>Lists of current Bets</h1>
+<h1>Lists of current Bets(TODO)</h1>
 <p><input type="submit" value="Display" name="DisplayCurrBets"></p>
 <hr/>
-<h1>Lists of users placing on Bets</h1>
+<h1>Lists of users placing on Bets(TODO)</h1>
 <p><input type="submit" value="Display" name="DisplayUserPlaceBets"></p>
 <hr/>
-<h1>Delete users here</h1>
+<h1>Delete users here(TODO)</h1>
 
-<form action="adminView.html" method="post">
+<form action="admin.php" method="post">
     <!--    should on cascade delete-->
     <label for="username">UserName to Delete:</label>
     <input type="text" id="username" name="username" placeholder="userName here">
     <input type="submit" value="Submit">
 </form>
+
+<?php
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = NULL; // edit the login credentials in connectToDB()
+
+
+function executePlainSQL($cmdstr)
+{ //takes a plain (no bound variables) SQL command and executes it
+    global $db_conn, $success;
+    $statement = oci_parse($db_conn, $cmdstr);
+    //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    $r = oci_execute($statement);
+    if (!$r) {
+        echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+        $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    return $statement;
+}
+
+function printResult($result)
+{ //prints results from a select statement
+    echo "<br>Retrieved data from table demoTable:<br>";
+    echo "<table>";
+    echo "<tr><th>UserName</th><th>AccountBalance</th><th>Email</th></tr>";
+    while ($row = oci_fetch_array($result, OCI_BOTH)) {
+        echo "<tr><td>" . $row[0] . "</td><td>" . $row[1] . "</td><td>" . $row[2] . "</td></tr>";
+    }
+    echo "</table>";
+}
+
+function displayUsers()
+{
+    if (connectToDB()) {
+        printResult(executePlainSQL("SELECT * FROM GeneralUser"));
+        disconnectFromDB();
+    }
+}
+
+function connectToDB()
+{
+    global $db_conn;
+    // Your username is ora_(CWL_ID) and the password is a(student number). For example,
+    // ora_platypus is the username and a12345678 is the password.
+    $db_conn = oci_connect("ora_yukiny", "a13215942", "dbhost.students.cs.ubc.ca:1522/stu");
+
+    if ($db_conn) {
+        return true;
+    } else {
+        $e = OCI_Error(); // For OCILogon errors pass no handle
+        echo htmlentities($e['message']);
+        return false;
+    }
+}
+
+function disconnectFromDB()
+{
+    global $db_conn;
+    oci_close($db_conn);
+}
+
+if (isset($_GET['DisplayCurrUsersRequest'])) {
+    displayUsers();
+}
+
+?>
 
 </body>
 </html>
--- main.php
+++ main.php
@@ -1,43 +1,129 @@
-<html> 
-  <head>
-    <title>Home Page</title> 
-  </head>
-  <body> 
-    <h1>NBA Betting</h1> 
-    <p>Let's win some money!</p>
-    <hr/>
-    <h1>Sign up here</h1>
-    <form action="generalUser.php" method="post">
-        <!--    should add the account to database
-                should check if user is registered already
-                createUser logic
-        -->
-        <label for="username">Username:</label>
-        <input type="text" id="newUsername" name="username" placeholder="Enter your username">
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+<h1>NBA Betting</h1>
+<p>Let's win some money!</p>
+<hr/>
+<h1>Sign up here (Working?)</h1>
+<form action="" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="newUsername" name="SignUpUsername" placeholder="Enter your username">
+    <label for="email">Email:</label>
+    <input type="email" id="newEmail" name="SignUpEmail" placeholder="Enter your email">
+    <input type="submit" value="Submit" name="CreateNewUser">
+</form>
+<hr/>
+<h1>Login here (TODO)</h1>
+<form action="generalUser.php" method="post">
+    <!--   popup if user not found -->
+    <!--   add login logic -->
+    <!--   should log in with correct accountBalance, email-->
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="LoginUsername" placeholder="Enter your username">
 
-        <label for="email">Email:</label>
-        <input type="email" id="newEmail" name="email" placeholder="Enter your email">
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="LoginEmail" placeholder="Enter your email">
 
-        <input type="submit" value="Submit">
-    </form>
-    <hr/>
-    <h1>Login here</h1>
-    <form action="generalUser.php" method="post">
-        <!--   popup if user not found -->
-        <!--   add login logic -->
-        <!--   should login with correct accountBalance, email-->
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" placeholder="Enter your username">
+    <input type="submit" value="Submit">
+</form>
+<hr/>
+<h1>Click here to view as admin</h1>
+<form action="admin.php" method="post">
+    <input type="submit" value="LoginAsAdmin">
+</form>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Enter your email">
 
-        <input type="submit" value="Submit">
-    </form>
-    <hr/>
-    <h1>Click here to view as admin</h1>
-    <form action="admin.php" method="post">
-        <input type="submit" value="LoginAsAdmin">
-    </form>
-  </body>
+<?php
+// global variables
+$success = True; //keep track of errors so it redirects the page only if there are no errors
+$db_conn = NULL; // edit the login credentials in connectToDB()
+
+// executeBoundSQL, handle error
+function executeBoundSQL($cmdstr, $list)
+{
+    /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
+In this case you don't need to create the statement several times. Bound variables cause a statement to only be
+parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
+See the sample code below for how this function is used */
+    global $db_conn, $success;
+    $statement = oci_parse($db_conn, $cmdstr);
+
+    if (!$statement) {
+        echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+        $e = OCI_Error($db_conn);
+        echo htmlentities($e['message']);
+        $success = False;
+    }
+
+    foreach ($list as $tuple) {
+        foreach ($tuple as $bind => $val) {
+            //echo $val;
+            //echo "<br>".$bind."<br>";
+            oci_bind_by_name($statement, $bind, $val);
+            unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+        }
+
+        $r = oci_execute($statement);
+        if (!$r) {
+            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
+            echo htmlentities($e['message']);
+            echo "<br>";
+            $success = False;
+        } else {
+            echo "SignUp Success!";
+        }
+    }
+}
+
+// function connectToDB()
+function connectToDB()
+{
+    global $db_conn;
+    // Your username is ora_(CWL_ID) and the password is a(student number). For example,
+    // ora_platypus is the username and a12345678 is the password.
+    $db_conn = oci_connect("ora_yukiny", "a13215942", "dbhost.students.cs.ubc.ca:1522/stu");
+    if ($db_conn) {
+        return true;
+    } else {
+        $e = OCI_Error(); // For OCILogon errors pass no handle
+        echo htmlentities($e['message']);
+        return false;
+    }
+}
+
+
+// function disconnectDB
+function disconnectFromDB()
+{
+    global $db_conn;
+    oci_close($db_conn);
+}
+
+// function handleCreatNewUserRequest -> call executeBoundSql
+function handleCreateNewUserRequest()
+{
+    global $db_conn;
+    if (connectToDB()) {
+        $tuple = array (
+                ":bind1" => $_POST['SignUpUsername'],
+                ":bind2" => $_POST['SignUpEmail']
+        );
+        $alltuples = array ($tuple);
+        executeBoundSQL("insert into GeneralUser(UserName, Email) values(:bind1, :bind2)", $alltuples);
+        oci_commit($db_conn);
+        disconnectFromDB();
+    }
+}
+
+// if SignUp is pressed
+if (isset($_POST['CreateNewUser'])) {
+    handleCreateNewUserRequest();
+
+}
+
+?>
+</body>
 </html>
