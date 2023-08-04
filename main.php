14a7b6f0d0ba04513ff92d4a39e7d0d2eb4adb09
Naomi Tung <ntung34@pender.students.cs.ubc.ca>
2023-08-03 21:37:04 -0700

    changed sign up on main.php to button

--- main.php
+++ main.php
@@ -11,12 +11,13 @@
 <p>Let's win some money!</p>
 <hr/>
 <h1>Sign up here</h1>
-<form action="" method="post">
-    <label for="username">Username:</label>
-    <input type="text" id="newUsername" name="SignUpUsername" placeholder="Enter your username" required>
-    <label for="email">Email:</label>
-    <input type="email" id="newEmail" name="SignUpEmail" placeholder="Enter your email" required>
-    <input type="submit" value="Submit" name="CreateNewUser">
+<form action="signup.php" method="post">
+    <input type="submit" value="Click Here">
+    // <label for="username">Username:</label>
+    // <input type="text" id="newUsername" name="SignUpUsername" placeholder="Enter your username" required>
+    // <label for="email">Email:</label>
+    // <input type="email" id="newEmail" name="SignUpEmail" placeholder="Enter your email" required>
+    // <input type="submit" value="Submit" name="CreateNewUser">
 </form>
 <hr/>
 <h1>Login here</h1>
--- signup.php
+++ signup.php
@@ -7,6 +7,12 @@
     <div>
         <h1>Sign Up Here!</h1>
     </div>  
+    <form>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname">
+    </form>
 </body>
 
 
