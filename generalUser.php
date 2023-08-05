d3bba70e98b7c238e101143e14ee3882327e83b1
black <black@students.cs.ubc.ca>
2023-08-04 21:23:47 -0700

    Added selection to displaygames

--- generalUser.php
+++ generalUser.php
@@ -99,6 +99,17 @@ function printGames($result)
 function displayGames()
 {
     if (connectToDB()) {
+        string command = "SELECT
+        G.GameID,
+        G.ScoreHome,
+        G.ScoreAway,
+        G.GameDate,
+        THome.FullName AS HomeTeam,
+        TAway.FullName AS AwayTeam
+        FROM
+        Game G
+        INNER JOIN Team THome ON G.HomeTeamID = THome.TeamID
+        INNER JOIN Team TAway ON G.AwayTeamID = TAway.TeamID;"
         printGames(executePlainSQL("SELECT * FROM Game"));
         disconnectFromDB();
     }
