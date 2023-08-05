b748ab9be4f12d89b6286b99205d075895e6b64e
UBC Student <black@students.cs.ubc.ca>
2023-08-05 16:20:02 -0700

    Update insert.sql
    Added Team Insertions

--- sql/insert.sql
+++ sql/insert.sql
@@ -13,3 +13,34 @@ VALUES (0022200608, 121, 113, TO_DATE('2023-01-09', 'YYYY-MM-DD'), 1610612763, 1
 
 INSERT INTO Game (GameID, ScoreHome, ScoreAway, GameDate, HomeTeamID, AwayTeamID)
 VALUES (2022200086, 104, 112, TO_DATE('2023-01-09', 'YYYY-MM-DD'), 1612709914, 1612709904);
+
+INSERT INTO team VALUES('1610612737','Atlanta Hawks','Atlanta');
+INSERT INTO team VALUES('1610612738','Boston Celtics','Boston');
+INSERT INTO team VALUES('1610612739','Cleveland Cavaliers','Cleveland');
+INSERT INTO team VALUES('1610612740','New Orleans Pelicans','New Orleans');
+INSERT INTO team VALUES('1610612741','Chicago Bulls','Chicago');
+INSERT INTO team VALUES('1610612742','Dallas Mavericks','Dallas');
+INSERT INTO team VALUES('1610612743','Denver Nuggets','Denver');
+INSERT INTO team VALUES('1610612744','Golden State Warriors','Golden State');
+INSERT INTO team VALUES('1610612745','Houston Rockets','Houston');
+INSERT INTO team VALUES('1610612746','Los Angeles Clippers','Los Angeles');
+INSERT INTO team VALUES('1610612747','Los Angeles Lakers','Los Angeles');
+INSERT INTO team VALUES('1610612748','Miami Heat','Miami');
+INSERT INTO team VALUES('1610612749','Milwaukee Bucks','Milwaukee');
+INSERT INTO team VALUES('1610612750','Minnesota Timberwolves','Minnesota');
+INSERT INTO team VALUES('1610612751','Brooklyn Nets','Brooklyn');
+INSERT INTO team VALUES('1610612752','New York Knicks','New York');
+INSERT INTO team VALUES('1610612753','Orlando Magic','Orlando');
+INSERT INTO team VALUES('1610612754','Indiana Pacers','Indiana');
+INSERT INTO team VALUES('1610612755','Philadelphia 76ers','Philadelphia');
+INSERT INTO team VALUES('1610612756','Phoenix Suns','Phoenix');
+INSERT INTO team VALUES('1610612757','Portland Trail Blazers','Portland');
+INSERT INTO team VALUES('1610612758','Sacramento Kings','Sacramento');
+INSERT INTO team VALUES('1610612759','San Antonio Spurs','San Antonio');
+INSERT INTO team VALUES('1610612760','Oklahoma City Thunder','Oklahoma City');
+INSERT INTO team VALUES('1610612761','Toronto Raptors','Toronto');
+INSERT INTO team VALUES('1610612762','Utah Jazz','Utah');
+INSERT INTO team VALUES('1610612763','Memphis Grizzlies','Memphis');
+INSERT INTO team VALUES('1610612764','Washington Wizards','Washington');
+INSERT INTO team VALUES('1610612765','Detroit Pistons','Detroit');
+INSERT INTO team VALUES('1610612766','Charlotte Hornets','Charlotte');
