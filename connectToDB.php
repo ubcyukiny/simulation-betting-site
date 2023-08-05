1ef4cff1fed51134ed87b12e3da6dfd59ccbfc1b
black <black@students.cs.ubc.ca>
2023-08-05 16:49:54 -0700

    Reverted to previous functions.php / connectoDB split

--- connectToDB.php
+++ connectToDB.php
@@ -1,11 +1,20 @@
 <?php
 $global_success = True; //keep track of errors so it redirects the page only if there are no errors
-$global_db_conn = NULL; // edit the login credentials in connectToDB()
 
-function get_db_conn()
+function connectToDB()
 {
+    global $global_db_conn;
     // Your username is ora_(CWL_ID) and the password is a(student number). For example,
     // ora_platypus is the username and a12345678 is the password.
 
-    return oci_connect("ora_black", "password", "dbhost.students.cs.ubc.ca:1522/stu");
+    //$global_db_conn = oci_connect("ora_black", "password", "dbhost.students.cs.ubc.ca:1522/stu");
+    $global_db_conn = oci_connect("ora_black", "password", "dbhost.students.cs.ubc.ca:1522/stu");
+
+    if ($global_db_conn) {
+        return true;
+    } else {
+        $e = OCI_Error(); // For OCILogon errors pass no handle
+        echo htmlentities($e['message']);
+        return false;
+    }
 }
